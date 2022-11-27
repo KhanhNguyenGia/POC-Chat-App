12c6816a419bbb5b5e57c51babb0f4ef995e8bbe
KhanhGN <kgnguyen@myseneca.ca>
2022-11-27 19:56:14 +0700

    PWA

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,29 @@ export default defineConfig({
 		federation({
 			name: '',
 		}),
-		VitePWA({ registerType: 'autoUpdate' }),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+			manifest: {
+				name: 'MyChat App',
+				short_name: 'MC',
+				icons: [
+					{
+						src: '/android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+				theme_color: '#121212',
+				background_color: '#121212',
+				display: 'standalone',
+				start_url: '/',
+			},
+		}),
 	],
 });
